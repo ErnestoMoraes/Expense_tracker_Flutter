4eef1e7077c75ff9ff82ab4a02bc088d3e882908
ErnestoMoraes <joseernestosantosmoraes@gmail.com>
2023-02-18 10:11:12 -0300

    Introduzindo o convert e os dtos

---
+++ lib/data/expanse_data.dart
@@ -0,0 +1,58 @@
+import 'package:expense_tracker/datetime/date_time_helper.dart';
+import 'package:expense_tracker/expense/expense_item.dart';
+
+class ExpanseData {
+  List<ExpenseItem> overallExpenseList = [];
+
+  List<ExpenseItem> getAllExpanseList() {
+    return overallExpenseList;
+  }
+
+  void addNewExpense(ExpenseItem newExpense) {
+    overallExpenseList.add(newExpense);
+  }
+
+  void deleteExpanse(ExpenseItem expense) {
+    overallExpenseList.remove(expense);
+  }
+
+  String getDayName(DateTime dateTime) {
+    switch (dateTime.weekday) {
+      case 1:
+        return 'Mon';
+      case 2:
+        return 'Tue';
+      case 3:
+        return 'Wed';
+      case 4:
+        return 'Thur';
+      case 5:
+        return 'Fri';
+      case 6:
+        return 'Sat';
+      case 7:
+        return 'Sun';
+      default:
+        return '';
+    }
+  }
+
+  Map<String, double> calculateDailyExpenseSummary() {
+    Map<String, double> dailyExpenseSummary = {
+      // date (yyyymmdd) : amountTotalForDay
+    };
+    for (var expense in overallExpenseList) {
+      String date = convertDateTimeToString(expense.dateTime);
+      double amount = double.parse(expense.amount);
+
+      if (dailyExpenseSummary.containsKey(date)) {
+        double currentAmount = dailyExpenseSummary[date]!;
+        currentAmount += amount;
+        dailyExpenseSummary[date] = currentAmount;
+      } else {
+        dailyExpenseSummary.addAll({date: amount});
+      }
+    }
+    return dailyExpenseSummary;
+  }
+}
