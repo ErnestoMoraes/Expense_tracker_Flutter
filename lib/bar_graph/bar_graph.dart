60a889aeb9885bf518634af4663929671ac6d114
ErnestoMoraes <joseernestosantosmoraes@gmail.com>
2023-02-18 12:58:12 -0300

    Fazendo Graph

---
+++ lib/bar_graph/bar_graph.dart
@@ -0,0 +1,54 @@
+import 'package:expense_tracker/bar_graph/bar_data.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+
+class BarGraph extends StatelessWidget {
+  final double? maxY;
+  final double sunAmount;
+  final double monAmount;
+  final double tueAmount;
+  final double wedAmount;
+  final double thurAmount;
+  final double friAmount;
+  final double satAmount;
+
+  const BarGraph({
+    super.key,
+    required this.maxY,
+    required this.sunAmount,
+    required this.monAmount,
+    required this.tueAmount,
+    required this.wedAmount,
+    required this.thurAmount,
+    required this.friAmount,
+    required this.satAmount,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    BarData myBarData = BarData(
+      sunAmount: sunAmount,
+      monAmount: monAmount,
+      tueAmount: tueAmount,
+      wedAmount: wedAmount,
+      thurAmount: thurAmount,
+      friAmount: friAmount,
+      satAmount: satAmount,
+    );
+    myBarData.initializedBarData();
+
+    return BarChart(BarChartData(
+      maxY: maxY,
+      minY: 0,
+      barGroups: myBarData.barData
+          .map(
+            (data) => BarChartGroupData(x: data.x, barRods: [
+              BarChartRodData(
+                toY: data.y,
+              )
+            ]),
+          )
+          .toList(),
+    ));
+  }
+}
---
+++ lib/components/expense_sumary.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ExpenseSumary extends StatelessWidget {
+  const ExpenseSumary({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
