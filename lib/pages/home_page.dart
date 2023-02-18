b989b5a7ef8c23dd99628c1acae47ce37fd44db8
ErnestoMoraes <joseernestosantosmoraes@gmail.com>
2023-02-18 10:42:48 -0300

    Showdialog

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,15 +1,62 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final nameEC = TextEditingController();
+  final amountEC = TextEditingController();
+
+  void addNewExpense() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Add new expense'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: nameEC,
+            ),
+            TextField(
+              controller: amountEC,
+            ),
+          ],
+        ),
+        actions: [
+          MaterialButton(
+            onPressed: save,
+            child:const  Text('Save'),
+          ),
+          MaterialButton(
+            onPressed: cancel,
+            child: const Text('Cancel'),
+          )
+        ],
+      ),
+    );
+  }
+
+  void save() {}
+  void cancel() {}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(''),
+        title: const Text('Expense Tracker'),
+        centerTitle: true,
       ),
       body: Container(),
+      backgroundColor: Colors.grey[300],
+      floatingActionButton: FloatingActionButton(
+        onPressed: addNewExpense,
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
